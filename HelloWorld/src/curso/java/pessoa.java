a3def5008a1e58ea9483d1c10139379148d53744
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-05-05 23:40:35 -0300

    Adicionando campo e classe de idade

--- HelloWorld/src/curso/java/pessoa.java
+++ HelloWorld/src/curso/java/pessoa.java
@@ -5,6 +5,16 @@ public class pessoa {
 	private String nome;
 		
 	private String sobrenome;
+	
+	private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
