58c6ae41d403cc3ef34d60915d1c70b8ddb8512d
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-05-12 19:11:48 -0300

    Adicionando minha 2 classe e linhas de codigo com variaveis

---
+++ HelloWorld/src/curso/java/SegundaClasse.java
@@ -0,0 +1,34 @@
+package curso.java;
+
+public class SegundaClasse {
+
+	static int idade = 14;
+	
+	static String nome = "Pedro Lucas";
+	
+	static String cpf = "187.987.123-54";
+	
+	public static void main(String[] agrs) {
+		int idosoIdade = 60;
+		
+		int jovemIdade = 18;
+		
+		int adolescenteIdade = 16;
+		
+		int criancaIdade = 7;
+		
+		
+		metodo1();
+		
+	}
+	
+	
+		public static void metodo1 () {
+			System.out.println("O nome da Pessoa é = " + nome);
+			System.out.println("A idade da Pessoa é = " + idade);
+			System.out.println("O CPF da Pessoa é = " + cpf);
+		}
+		
+	
+	
+}
