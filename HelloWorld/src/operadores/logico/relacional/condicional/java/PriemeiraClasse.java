f720d8794fbc5ed7b6dd6eb3c9421fe1176ce4db
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-06-07 11:31:56 -0300

    Aprimoramento do Codigo da Primeira Classe

--- HelloWorld/src/operadores/logico/relacional/condicional/java/PriemeiraClasse.java
+++ HelloWorld/src/operadores/logico/relacional/condicional/java/PriemeiraClasse.java
@@ -1,23 +1,30 @@
 package operadores.logico.relacional.condicional.java;
 
 public class PriemeiraClasse {
-	
+
 	public static void main(String[] args) {
-		
-		double nota1 = 6.0;	
-		double nota2 = 7.7;
-		double nota3 = 5.8;
-		double media = 0.0;
-		
-		media = (nota1 + nota2 + nota3) / 4;
-		
-		if (media >= 7.0) {
-			System.out.println("Aluno Aprovado: " + media);
-		}else if (media >= 4.0 && media <= 6.9) {
-			System.out.println("Aluno de Recuperação: " + media);
-		}else {
-			System.out.println("Aluno Reprovado" + media);
+
+		int nota1 = 100;
+		int nota2 = 80;
+		int nota3 = 78;
+		int media = 0;
+
+		media = (nota1 + nota2 + nota3) / 3;
+
+		/* Condições Logicas com IF e ELSE */
+		if (media >= 70) {
+			System.out.println("Media do Aluno: " + media);
+		} else if (media >= 40 && media <= 69) {
+			System.out.println("Media do Aluno: " + media);
+		} else {
+			System.out.println("Media do Aluno: " + media);
 		}
+		
+		/*Operadores tenários são para micro validações*/
+		
+		String mediaFinal = media >= 70 ? "Aprovado no Bimestre" : "Reprovado no Bimestre"; 
+		
+		System.out.println(mediaFinal);
 		
 	}
 
