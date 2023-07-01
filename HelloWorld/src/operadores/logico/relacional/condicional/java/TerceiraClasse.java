b63dd4b63288e564350faabb63a04807f02faecd
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-07-01 15:25:48 -0300

    Adicionando Terceira Classe e Codigo com SWITCH CASE

---
+++ HelloWorld/src/operadores/logico/relacional/condicional/java/TerceiraClasse.java
@@ -0,0 +1,41 @@
+package operadores.logico.relacional.condicional.java;
+
+public class TerceiraClasse {
+	
+	public static void main(String[] args) {
+		
+		/*SWITCH CASE: OPERAÇÕES EXATAS*/
+		
+		int dia = 10;
+		
+		switch (dia) {
+		case 1: 
+				System.out.println("Hoje é Domingo: Final de Semana Ebaaa!!");
+			break;
+		case 2: 
+				System.out.println("Hoje é Segunda-Feira");
+			break;
+		case 3: 
+				System.out.println("Hoje é Terca-Feira");
+			break;
+		case 4:
+				System.out.println("Hoje é Quarta-Feira");
+			break;
+		case 5: 
+				System.out.println("Hoje é Quinta-Feira");
+			break;
+		case 6: 
+				System.out.println("Hoje é Sexta-Feira: Ebaaa!!!");
+			break;
+		case 7: 
+				System.out.println("Hoje é Sabado: Final de Semana Ebaaa!!!");
+			break;
+		
+		default:
+				System.out.println("Dia Não Encontrado no Banco de Dados");
+			break;
+		}
+		
+	}	
+
+}
