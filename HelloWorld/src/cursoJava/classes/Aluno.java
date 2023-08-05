e870eb9d5385c09722722cfdf6d313f15c6ddecf
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-08-05 16:51:33 -0300

    Criação de novos packges executavel e classes, (Orientação a Objetos)

---
+++ HelloWorld/src/cursoJava/classes/Aluno.java
@@ -0,0 +1,30 @@
+package cursoJava.classes;
+
+public class Aluno {
+	
+	/*Esses são os atributos do Aluno*/
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMaticulado;
+	
+	public Aluno() { /*Cria os dados na memoria do java por padrão*/
+		
+	}
+	
+	public Aluno (String nomePadrao) {
+		nome = nomePadrao;
+	}
+	
+	public Aluno (String nomePadrao, int idadePadrao) {
+		nome = nomePadrao;
+		idade = idadePadrao;
+	}
+}
---
+++ HelloWorld/src/cursoJava/executavel/Alunos.java
@@ -0,0 +1,24 @@
+package cursoJava.executavel;
+
+import cursoJava.classes.Aluno;
+
+public class Alunos {
+
+	public static void main(String[] args) {
+		
+		/*new Aluno() é uma instancia (Criação de Objetos)*/
+		/*as variaveis são uma referencia para o objeto Aluno*/
+		
+		Aluno aluno1 = new Aluno(); /*Aqui será Pedro*/
+		
+		Aluno aluno2 = new Aluno(); /*Aqui será Wyltamar*/
+		
+		Aluno aluno3 = new Aluno(); /*Aqui será Yuri*/
+		
+		Aluno aluno4 = new Aluno("Amanda");
+		
+		Aluno aluno5 = new Aluno("José", 58);
+
+	}
+
+}
