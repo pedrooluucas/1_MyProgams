31a51cd3f6205085eb4dd9bdeb5e0f8e96e94500
Pedro Lucas <pedrooluucas2010@gmail.com>
2023-08-27 15:07:56 -0300

    Adicionando metodos Get and Setters e fazendo pequeno caucule do notas

--- HelloWorld/src/cursoJava/classes/Aluno.java
+++ HelloWorld/src/cursoJava/classes/Aluno.java
@@ -4,16 +4,20 @@ public class Aluno {
 	
 	/*Esses são os atributos do Aluno*/
 	
-	public String nome;
-	int idade;
-	String dataNascimento;
-	String registroGeral;
-	String numeroCpf;
-	String nomeMae;
-	String nomePai;
-	String dataMatricula;
-	String nomeEscola;
-	String serieMaticulado;
+	private String nome;
+	private int idade;
+	private	String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMaticulado;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
 	
 	public Aluno() { /*Cria os dados na memoria do java por padrão*/
 		
@@ -27,4 +31,133 @@ public class Aluno {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMaticulado() {
+		return serieMaticulado;
+	}
+
+	public void setSerieMaticulado(String serieMaticulado) {
+		this.serieMaticulado = serieMaticulado;
+	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
+
+	
+	public double getMediaBimestre() {
+		return (nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	
+	public boolean getAlunoAprovado() {
+		double media = this.getMediaBimestre();
+		if (media >= 7.0) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
 }
--- HelloWorld/src/cursoJava/executavel/Alunos.java
+++ HelloWorld/src/cursoJava/executavel/Alunos.java
@@ -11,9 +11,69 @@ public class Alunos {
 		
 		
 		Aluno aluno1 = new Aluno(); /*Aqui será Pedro*/
-		aluno1.nome = "Joao";
+		aluno1.setNome("Pedro Lucas Almeida de Oliveira");
+		aluno1.setIdade(13);
+		aluno1.setDataNascimento("28/01/2010");
+		aluno1.setRegistroGeral("1543.9878.6754");
+		aluno1.setNumeroCpf("1253.9847.6743.11");
+		aluno1.setNomeMae("Amanda Gonçalves de Almeida");
+		aluno1.setNomePai("Wyltamar Douglas de Sousa Oliveira");
+		aluno1.setDataMatricula("10/01/2020");
+		aluno1.setNomeEscola("E.M.E.F João Rosado de Oliveira");
+		aluno1.setSerieMaticulado("8 Ano");
+		aluno1.setNota1(9.5);
+		aluno1.setNota2(8.7);
+		aluno1.setNota3(7.4);
+		aluno1.setNota4(10.0);
+		
+		System.out.println("O Nome do Aluno é = " + aluno1.getNome());
+		System.out.println("A idade do Aluno é = " + aluno1.getIdade());
+		System.out.println("A Data de Nascimento do Aluno é = " + aluno1.getDataNascimento());
+		System.out.println("O RG do Aluno é = " + aluno1.getRegistroGeral());
+		System.out.println("O CPF do Aluno é = " + aluno1.getNumeroCpf());
+		System.out.println("A Mãe do Aluno é = " + aluno1.getNomeMae());
+		System.out.println("O Pai do Aluno é = " + aluno1.getNomePai());
+		System.out.println("A Data da Matricula do Aluno é = " + aluno1.getDataMatricula());
+		System.out.println("O Nome da Escola do Aluno é = " + aluno1.getNomeEscola());
+		System.out.println("A Serie Matriculada do Aluno é = " + aluno1.getSerieMaticulado());
+		System.out.println("A Media Bimestral do Aluno é: " + aluno1.getMediaBimestre());
+		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
+		
+		/*============================================================================================*/
+		
+		System.out.println("---------------------------------------------------------------------------");
 		
 		Aluno aluno2 = new Aluno(); /*Aqui será Wyltamar*/
+		aluno2.setNome("Wyltamar Douglas de Sousa Oliveira");
+		aluno2.setIdade(40);
+		aluno2.setDataNascimento("3/06/1997");
+		aluno2.setRegistroGeral("1543.9878.6754");
+		aluno2.setNumeroCpf("1253.9847.6743.11");
+		aluno2.setNomeMae("Maria do Socorro");
+		aluno2.setNomePai("Atonio Oliveira");
+		aluno2.setDataMatricula("10/07/2020");
+		aluno2.setNomeEscola("E.M JDEV Treinamento");
+		aluno2.setSerieMaticulado("3 Fase");
+		aluno2.setNota1(9.8);
+		aluno2.setNota2(8.5);
+		aluno2.setNota3(10.0);
+		aluno2.setNota4(10.0);
+		
+		System.out.println("O Nome do Aluno é = " + aluno2.getNome());
+		System.out.println("A idade do Aluno é = " + aluno2.getIdade());
+		System.out.println("A Data de Nascimento do Aluno é = " + aluno2.getDataNascimento());
+		System.out.println("O RG do Aluno é = " + aluno2.getRegistroGeral());
+		System.out.println("O CPF do Aluno é = " + aluno2.getNumeroCpf());
+		System.out.println("A Mãe do Aluno é = " + aluno2.getNomeMae());
+		System.out.println("O Pai do Aluno é = " + aluno2.getNomePai());
+		System.out.println("A Data da Matricula do Aluno é = " + aluno2.getDataMatricula());
+		System.out.println("O Nome da Escola do Aluno é = " + aluno2.getNomeEscola());
+		System.out.println("A Serie Matriculada do Aluno é = " + aluno2.getSerieMaticulado());
+		System.out.println("A Media Bimestral do Aluno é: " + aluno2.getMediaBimestre());
+		System.out.println("Resultado = " + (aluno2.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		
+		/*============================================================================================*/
 		
 		Aluno aluno3 = new Aluno(); /*Aqui será Yuri*/
 		
